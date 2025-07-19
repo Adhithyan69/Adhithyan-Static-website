2474ace7d4b8dd3189bd8af449b6db156129179f
MyLap <aadhiadhithyan001@gmail.com>
2025-07-19 16:39:14 +0530

    initial commit

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-// Animate Skills
+
 document.querySelectorAll('.skill').forEach(skill => {
   const percent = skill.getAttribute('data-percent');
   const fill = skill.querySelector('.skill-fill');
   fill.style.setProperty('--skill-level', percent + '%');
 });
 
-// Toggle mobile nav
+
 document.getElementById("hamburger").addEventListener("click", function () {
   document.getElementById("navLinks").classList.toggle("active");
   
